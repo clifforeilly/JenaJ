b9fe3311c9fe9c31b0cd36db998993721c5d400f
cliffo <clifforeilly@gmail.com>
2016-07-12 21:39:15 +0100

    Initial commit

---
+++ src/Control.java
@@ -0,0 +1,17 @@
+/**
+ * Created by cliff on 12/07/2016.
+ */
+public class Control {
+
+    public static void main (String[] args){
+
+        System.out.println("starting Control");
+
+        S1 s1 = new S1("One");
+
+
+        System.out.println("ending Control");
+
+    }
+
+}
---
+++ src/S1.java
@@ -0,0 +1,21 @@
+/**
+ * Created by cliff on 12/07/2016.
+ */
+public class S1 {
+
+    String type;
+
+    public S1(String Ptype)
+    {
+        System.out.println("starting Control");
+
+        type = Ptype;
+        System.out.println("Type=" + type);
+
+
+
+
+        System.out.println("ending Control");
+
+    }
+}
