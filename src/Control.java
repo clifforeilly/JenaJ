d21c117238469603fe4c125a4fa8b58bd6780863
cliffo <clifforeilly@gmail.com>
2016-07-12 23:01:00 +0100

    update

--- src/Control.java
+++ src/Control.java
@@ -1,17 +1,60 @@
+import edu.stanford.nlp.time.SUTime;
+import org.apache.jena.atlas.lib.ProgressMonitor;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by cliff on 12/07/2016.
  */
+
+
 public class Control {
+
+
+    static String OutputFolder;
+    static String NowD;
+    static String inputFileLoc;
 
     public static void main (String[] args){
 
-        System.out.println("starting Control");
+        try
+        {
+            System.out.println("starting Control");
 
-        S1 s1 = new S1("One");
+            OutputFolder = "D:\\LaRheto\\Outputs";
+            System.out.println("... OutputFolder=" + OutputFolder);
+
+            NowD = GetNow();
+            System.out.println("... Hash=" + NowD);
+
+            inputFileLoc = "D:\\LaRheto\\Inputs\\english-kjv.txt";
+            System.out.println("... FileLoc=" + inputFileLoc);
 
 
-        System.out.println("ending Control");
+            Parse parse = new Parse(OutputFolder, NowD);
+            System.out.println("... running Parse");
+            parse.run(inputFileLoc);
 
+
+
+
+            System.out.println("ending Control");
+        }
+        catch (Exception ee)
+        {
+            ee.printStackTrace();
+        }
+    }
+
+
+    public static String GetNow()
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        String n = sdf.format(new Date());
+        return n;
     }
 
 }
--- src/S1.java
+++ src/S1.java
@@ -1,6 +1,13 @@
 /**
  * Created by cliff on 12/07/2016.
  */
+
+import org.apache.jena.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.VCARD;
+
 public class S1 {
 
     String type;
@@ -12,8 +19,12 @@ public class S1 {
         type = Ptype;
         System.out.println("Type=" + type);
 
+        String personURI = "http://cliff";
+        String fullName = "Cliff O";
 
-
+        Model model = ModelFactory.createDefaultModel();
+        Resource cliffo = model.createResource(personURI);
+        cliffo.addProperty(VCARD.FN, fullName);
 
         System.out.println("ending Control");
 
